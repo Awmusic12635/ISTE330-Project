b9145bd718b7d4c606997a38dfa18b1fcb7959df
Awmusic12635 <alex@alexwacker.com>
2015-11-12 10:15:54 -0500

    update DB to working state
    Fix broken query builder and incorrect calling of creating the connection

--- DB.php
+++ DB.php
@@ -1,13 +1,32 @@
 <?php
 class DB{
-	private $dbh;
-	
+	private $username;
+	private $password;
+	private $host;
+	private $db;
 	function connect($host,$db,$username,$password){
 		try{
 			$dbh = new PDO("mysql:host=$host;dbname=$db",$username,$password);
 			$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			
+			$this->username = $username;
+			$this->password = $password;
+			$this->host = $host;
+			$this->db = $db;
+			
+			return true;
 		}
 		catch(PDOException $e){
+			echo $e->getMessage();
+			return false;
+		}
+	}
+	function getConnection(){
+		try{
+			$dbh = new PDO("mysql:host=" . $this->host.";dbname=".$this->db,$this->username,$this->password);
+			return $dbh;
+		
+		}catch(PDOException $e){
 			echo $e->getMessage();
 		}
 	}
@@ -27,7 +46,7 @@ class DB{
 			$new_array = array_merge($array1,$array2);	
 			$array_size=count($new_array);
 			for($x=0;$x<$new_array;$x+=2){
-				$commaList+=$new_array[$x] . "=" . $new_array[$x+1] . ",";
+				$commaList = $commandList . $new_array[$x] . "=" . $new_array[$x+1] . ",";
 			}
 		}
 		$commaList = trim($commaList, ',');	
@@ -35,37 +54,41 @@ class DB{
 	}
 	function buildQuery($type,$table,$columns,$where=null,$values=null){
 		$query="";
-		
-		if($query=="select"){
-			$query+="select ";
-			$query+=commaSeparate($columns);
-			$query+=" from " . $table;
+		if($type=="select"){
+			$query = $query . "select ";
+			$query = $query . $this->commaSeparate($columns);
+			$query = $query . " from " . $table;
 			
 			if($where !=null){
-				$query += "where " . $where;
+				$query = $query . " where " . $where;
 			}
 			return $query;
 		}else if ($type=="insert"){
-			$query+="insert into " . $table . " ";
-			$query+="(" . commaSeparate($columns) . ") values (" . commaSeparate($values) . ")";
+			$query = $query . "insert into " . $table . " ";
+			$query = $query . "(" . $this->commaSeparate($columns) . ") values (" . $this->commaSeparate($values) . ")";
 			return $query;
 		}else if($type == "update"){
-			$query+="update " . $table . " set ". commaSeparate($columns,$values) . $where;
+			$query = $query . "update " . $table . " set ". $this->commaSeparate($columns,$values) . " " . $where;
 			 return $query;
 		}else{
-			$query+="delete from " . $table;
+			echo "true";
+			$query = $query . "delete from " . $table;
 			
 			if($where !=null){
-				$query+=" where " . $where; 
+				$query = $query . " where " . $where; 
 			}
 			return $query;
 		}
 	}
 	function getData($table,$columns,$where=null){
 		try{
-			$query = buildQuery("select",$table,$columns,$where);
+			$dbh = $this->getConnection();
+			$query = $this->buildQuery("select",$table,$columns,$where);
 			$stmt = $dbh->prepare($query);
 			$stmt->execute();
+			$data=$stmt->fetchAll();
+			
+			return $data;
 		}
 		catch(PDOException $e){
 			echo $e->getMessage();
@@ -73,33 +96,45 @@ class DB{
 	}
 	function insertData($table,$columns,$values,$where=null){
 		try{
-			$query = buildQuery("insert",$table,$columns,$where,$values);
+			$dbh = $this->getConnection();
+			$query = $this->buildQuery("insert",$table,$columns,$where,$values);
 			$stmt = $dbh->prepare($query);
 			$stmt->execute();
+			
+			return true;
 		}
 		catch(PDOException $e){
 			echo $e->getMessage();
+			return false;
 		}
 	}
 	function updateData($table,$columns,$values,$where=null){
 		try{
-			$query = buildQuery("update",$table,$columns,$where,$values);
+			$dbh = $this->getConnection();
+			$query = $this->buildQuery("update",$table,$columns,$where,$values);
 			$stmt = $dbh->prepare($query);
 			$stmt->execute();
+			
+			return true;
 		}
 		catch(PDOException $e){
 			echo $e->getMessage();
+			return false;
 		}
 	}
 	function deleteData($table,$where=null){
 		try{
 			$columns=null;
-			$query = buildQuery("delete",$table,$columns,$where);
+			$dbh = $this->getConnection();
+			$query = $this->buildQuery("delete",$table,$columns,$where);
 			$stmt = $dbh->prepare($query);
 			$stmt->execute();
+			
+			return true;
 		}
 		catch(PDOException $e){
 			echo $e->getMessage();
+			return false;
 		}
 	}
 }
